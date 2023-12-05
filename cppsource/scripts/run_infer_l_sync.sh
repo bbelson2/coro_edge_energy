402c51c309aef3e8a7d36fcb3be39717b5eb0c06
Bruce Belson <bruce.belson@my.jcu.edu.au>
2023-12-05 11:58:06 +1000

    Update run_infer_l_sync.sh

--- cppsource/scripts/run_infer_l_sync.sh
+++ cppsource/scripts/run_infer_l_sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export PATH=/usr/local/clang_12.0.1/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/clang_12.0.1/lib:$LD_LIBRARY_PATH
 ~/repos/coro_edge_energy/cppsource/buildl/infer7 "$@"
